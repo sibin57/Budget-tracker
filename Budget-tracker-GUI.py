ce7ebd25286b20a0c654267e4f36d473638c5c0a
iSanSanich <96165855+iSanSanich@users.noreply.github.com>
2024-04-02 22:42:47 +0300

    Update Budget-tracker-GUI.py
    Кнопки Добавить и Посмотреть теперь имеют диалоговое окно, с текстовыми полями, в которые пользователю необходимо написать: 
    1)Через кнопку <Добавить>В случае нужды добавить кол-во денег и дату, в последствии программа уведомит об успешном запоминании ваших данных и внесет их в базу данных
    2)Через кнопку <Посмотреть> В случае если пользователю необходимо посмотреть на кол-во денег в какой-нибудь определенной дате, ему нужно непосредственно ввести нужную дату, и выведутся средства записанные в тот день

--- Budget-tracker-GUI.py
+++ Budget-tracker-GUI.py
@@ -1,11 +1,12 @@
 import sqlite3
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(884, 652)
+        #Основное окно для дальнейших манипуляций с базой данных
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.Buttons = QtWidgets.QWidget(self.centralwidget)
@@ -45,22 +46,75 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        #Диалоговое окно добавления записей в таблицу базы данных
+        self.commitDialog = QtWidgets.QDialog() 
+        self.commitDialog.setWindowTitle("Введите данные")
+        self.commitDialog.resize(300, 200)
+        self.form_layout = QtWidgets.QFormLayout(self.commitDialog)
+        self.date_field = QtWidgets.QLineEdit(self.commitDialog)
+        self.form_layout.addRow("Дата:", self.date_field)
+        self.money_field = QtWidgets.QLineEdit(self.commitDialog)
+        self.form_layout.addRow("Количество денег:", self.money_field)
+        self.button_box = QtWidgets.QDialogButtonBox(self.commitDialog)
+        self.button_box.commitButton = QtWidgets.QPushButton(self.commitDialog)
+        self.button_box.commitButton.setGeometry(QtCore.QRect(30, 65, 75, 23))
+        self.button_box.commitButton.clicked.connect(self.on_commit_button_clicked)
+        self.button_box.cancelButton = QtWidgets.QPushButton(self.commitDialog)
+        self.button_box.cancelButton.setGeometry(QtCore.QRect(30, 130, 75, 23))
+        self.button_box.cancelButton.clicked.connect(self.on_cancel_button_clicked)
+        self.form_layout.addWidget(self.button_box)
+        #Диалоговое окно просмотра записей с таблицы базы данных
+        self.watchDialog = QtWidgets.QDialog()
+        self.watchDialog.setWindowTitle("Введите данные")
+        self.watchDialog.resize(300, 200)
+        self.watch_form_layout = QtWidgets.QFormLayout(self.watchDialog)
+        self.watch_date_field = QtWidgets.QLineEdit(self.watchDialog)
+        self.watch_form_layout.addRow("Дата:", self.watch_date_field)
+        self.watch_button_box = QtWidgets.QDialogButtonBox(self.watchDialog)
+        self.watch_button_box.watchButton = QtWidgets.QPushButton(self.watchDialog)
+        self.watch_button_box.watchButton.setGeometry(QtCore.QRect(30, 65, 75, 23))
+        self.watch_button_box.watchButton.clicked.connect(self.on_watchFromTable_button_clicked)
+        self.watch_button_box.cancelWatchButton = QtWidgets.QPushButton(self.watchDialog)
+        self.watch_button_box.cancelWatchButton.setGeometry(QtCore.QRect(30, 130, 75, 23))
+        self.watch_button_box.cancelWatchButton.clicked.connect(self.on_cancelWatch_button_clicked)
+        self.watch_form_layout.addWidget(self.watch_button_box)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def on_add_button_clicked(self):
+    def on_add_button_clicked(self): #К первой кнопке
+        self.commitDialog.show()
+        self.on_commit_button_clicked
+        
+    def on_commit_button_clicked(self): #К первой кнопке
         connection = sqlite3.connect("timeMoney.db")
-        addToTable(connection)
+        date = self.date_field.text()
+        money = self.money_field.text()
+        data_tuple = (date, money)
+        addToTable(connection, data_tuple)
         ui.textBrowser.append("Успешно добавлено!")
+        self.commitDialog.close()
 
-    def out_of_button_clicked(self):
+    def on_cancel_button_clicked(self): #К первой кнопке
+        self.commitDialog.close()
+    
+    def on_cancelWatch_button_clicked(self): #Ко второй кнопке
+        self.watchDialog.close()
+    
+    def on_watchFromTable_button_clicked(self):#Ко второй кнопке
         connection = sqlite3.connect("timeMoney.db")
-        outOfTable(connection)
-        receiveData = outOfTable(connection)
+        date = self.watch_date_field.text()
+        data_tuple = (date,)
+        outOfTable(connection, data_tuple)
+        receiveData = outOfTable(connection, data_tuple)
         formattedData = "\n".join(map(str, receiveData))
         ui.textBrowser.setText(formattedData)
         connection.close()
+
+    def out_of_button_clicked(self): #Ко второй кнопке
+        self.watchDialog.show()
+        self.on_watchFromTable_button_clicked
+        
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -69,28 +123,32 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "Посмотреть"))
         self.pushButton_3.setText(_translate("MainWindow", "Изменить"))
         self.pushButton_4.setText(_translate("MainWindow", "Удалить"))
+        self.button_box.commitButton.setText(_translate("MainWindow", "Записать"))
+        self.button_box.cancelButton.setText(_translate("MainWindow", "Отмена"))
+        self.watch_button_box.watchButton.setText(_translate("MainWindow", "Применить"))
+        self.watch_button_box.cancelWatchButton.setText(_translate("MainWindow", "Отмена"))
 
 
-def addToTable(connection):
+def addToTable(connection, data_tuple):
     try:
         cursor = connection.cursor()
     except sqlite3.Error as e:
-        print(f"Ошибка подключения к базе данных: {e}")
+        print(f"Ошибка подключения к базе данных: {e}")    
 
-    cursor.execute('INSERT INTO Tracking (date, money) values ("2016-02-06", 3450)')
+    cursor.execute("""INSERT INTO Tracking (date, money) values (?, ?)""", data_tuple)
     connection.commit()
     connection.close()
 
-def outOfTable(connection):
+def outOfTable(connection, data_tuple):
     try:
         cursor = connection.cursor()
     except sqlite3.Error as e:
-        print(f"Ошибка подключения к базе данных: {e}")
-    cursor.execute("SELECT (money, date) FROM Tracking")
+        print(f"Ошибка подключения к базе данных: {e}")  
+    cursor.execute("""SELECT money, date FROM Tracking WHERE date = ?""", data_tuple)
     info = cursor.fetchall()
     connection.commit()
     return info
-
+    
 
 if __name__ == "__main__":
     import sys
